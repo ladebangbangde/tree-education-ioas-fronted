98569e1b931e84ba7575cf2fe5edf64c77370205
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-06-02 12:03:37 +0800

    Hide mock logs page

--- src/pages/settings/LogsPage.tsx
+++ src/pages/settings/LogsPage.tsx
@@ -1,23 +1,5 @@
-import { Button, DatePicker, Form, Input, Select, Space } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import { DataTable, PageHeader, SearchFilterBar } from '@/components';
-import { logs } from '@/mock/settings';
+import FeatureClosedPage from '@/pages/common/FeatureClosedPage';
 
 export default function LogsPage(){
-  const nav=useNavigate();
-  const columns=[{title:'操作人',dataIndex:'operator'},{title:'模块',dataIndex:'module'},{title:'操作类型',dataIndex:'type'},{title:'描述',dataIndex:'desc'},{title:'IP',dataIndex:'ip'},{title:'时间',dataIndex:'time'},{title:'结果',dataIndex:'result'},{title:'操作',render:(_:unknown,r:any)=><Space><Button type='link' onClick={()=>nav(`/settings/opLog/detail/${r.id}`)}>操作日志详情</Button><Button type='link' onClick={()=>nav(`/settings/loginLog/detail/${r.id}`)}>登录日志详情</Button></Space>}];
-  return <>
-    <PageHeader title='审计日志'/>
-    <SearchFilterBar>
-      <Form layout='inline'>
-        <Form.Item label='操作人'><Input/></Form.Item>
-        <Form.Item label='模块'><Select style={{width:140}} options={[{value:'线索中心'},{value:'学生档案'},{value:'CMS文章'}]}/></Form.Item>
-        <Form.Item label='操作类型'><Select style={{width:140}} options={[{value:'分配'},{value:'更新'},{value:'发布'}]}/></Form.Item>
-        <Form.Item label='结果'><Select style={{width:120}} options={[{value:'成功'},{value:'失败'}]}/></Form.Item>
-        <Form.Item label='时间范围'><DatePicker.RangePicker/></Form.Item>
-        <Button type='primary'>查询</Button><Button>重置</Button>
-      </Form>
-    </SearchFilterBar>
-    <DataTable rowKey='id' columns={columns} dataSource={logs}/>
-  </>
+  return <FeatureClosedPage title={'Audit Logs'} />;
 }
