b754da1975ae9218793f4f634dafa972381e90a8
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-06-02 11:56:16 +0800

    Hide mock data permission page

--- src/pages/settings/DataPermissionPage.tsx
+++ src/pages/settings/DataPermissionPage.tsx
@@ -1,7 +1,5 @@
-import { Button, Card, Checkbox, Col, Form, Row, Select, Space, Tag } from 'antd';
-import { PageHeader } from '@/components';
-import { dataPermissionScopes } from '@/mock/settings';
+import FeatureClosedPage from '@/pages/common/FeatureClosedPage';
 
 export default function DataPermissionPage(){
-  return <><PageHeader title='数据权限配置' extra={<Space><Button>查看变更记录</Button><Button type='primary'>保存配置</Button></Space>} /><Row gutter={[16,16]}><Col span={10}><Card title='权限范围'><Form layout='vertical'><Form.Item label='适用角色'><Select defaultValue='CONSULTANT' options={['SUPER_ADMIN','OPERATOR','CONSULTANT'].map(value=>({value}))}/></Form.Item><Form.Item label='可见部门'><Checkbox.Group options={dataPermissionScopes.departments} defaultValue={['咨询一部']}/></Form.Item><Form.Item label='可见业务'><Checkbox.Group options={dataPermissionScopes.modules} defaultValue={['线索中心','学生档案']}/></Form.Item></Form></Card></Col><Col span={14}><Card title='配置说明'>{dataPermissionScopes.rules.map(rule=><p key={rule}><Tag color='blue'>规则</Tag>{rule}</p>)}</Card></Col></Row></>;
+  return <FeatureClosedPage title={'\u6570\u636e\u6743\u9650\u914d\u7f6e'} />;
 }
