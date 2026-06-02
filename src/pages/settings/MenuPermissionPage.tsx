3e23e0de7349eab8754de8287ecb481cfd069986
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-06-02 11:56:39 +0800

    Hide mock menu permission page

--- src/pages/settings/MenuPermissionPage.tsx
+++ src/pages/settings/MenuPermissionPage.tsx
@@ -1,7 +1,5 @@
-import { Button, Card, Col, Row, Space, Table, Tree } from 'antd';
-import { PageHeader } from '@/components';
-import { menuPermissionTree, menuPermissionRows } from '@/mock/settings';
+import FeatureClosedPage from '@/pages/common/FeatureClosedPage';
 
 export default function MenuPermissionPage(){
-  return <><PageHeader title='菜单权限配置' extra={<Space><Button>恢复默认</Button><Button type='primary'>保存菜单权限</Button></Space>} /><Row gutter={[16,16]}><Col span={8}><Card title='菜单树'><Tree checkable defaultExpandAll defaultCheckedKeys={['dashboard','leads','cms']} treeData={menuPermissionTree}/></Card></Col><Col span={16}><Card title='按钮权限矩阵'><Table rowKey='key' pagination={false} dataSource={menuPermissionRows} columns={[{title:'模块',dataIndex:'module'},{title:'查看',dataIndex:'view'},{title:'新增',dataIndex:'create'},{title:'编辑',dataIndex:'edit'},{title:'导出',dataIndex:'export'}]} /></Card></Col></Row></>;
+  return <FeatureClosedPage title={'\u83dc\u5355\u6743\u9650\u914d\u7f6e'} />;
 }
