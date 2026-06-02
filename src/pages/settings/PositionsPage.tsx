473c6c11806382b5dabe5a7862daffc3cd24fc09
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-06-02 11:59:57 +0800

    Hide mock positions page

--- src/pages/settings/PositionsPage.tsx
+++ src/pages/settings/PositionsPage.tsx
@@ -1,7 +1,5 @@
-import { Button, Card, Col, Row, Space, Tag } from 'antd';
-import { PageHeader } from '@/components';
-import { positions } from '@/mock/settings';
+import FeatureClosedPage from '@/pages/common/FeatureClosedPage';
 
 export default function PositionsPage(){
-  return <><PageHeader title='岗位管理' extra={<Space><Button>岗位排序</Button><Button type='primary'>新增岗位</Button></Space>} /><Row gutter={[16,16]}>{positions.map(item=><Col span={8} key={item.id}><Card title={item.name} extra={<Tag color='blue'>{item.grade}</Tag>}><p>所属部门：{item.dept}</p><p>权限模板：{item.permission}</p><p>在岗人数：{item.members}</p></Card></Col>)}</Row></>;
+  return <FeatureClosedPage title={'Position Management'} />;
 }
