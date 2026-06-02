97c8e06bc655ccecbb7d1fcf43c07e035c0db48b
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-06-02 11:55:12 +0800

    Localize closed feature page

--- src/pages/common/FeatureClosedPage.tsx
+++ src/pages/common/FeatureClosedPage.tsx
@@ -1,9 +1,11 @@
-import { Card } from 'antd';
+import { Card, Empty } from 'antd';
 import { PageHeader } from '@/components';
 
-export default function FeatureClosedPage({ title = 'Closed' }: { title?: string }) {
+export default function FeatureClosedPage({ title = '\u8be5\u529f\u80fd\u672a\u5f00\u653e' }: { title?: string }) {
   return <>
     <PageHeader title={title} />
-    <Card>Closed</Card>
+    <Card>
+      <Empty description='\u8be5\u529f\u80fd\u672a\u5f00\u653e' />
+    </Card>
   </>;
 }
