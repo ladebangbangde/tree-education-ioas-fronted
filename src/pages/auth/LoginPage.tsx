15bfa42479a1b2986e0da93210bab2977bcad81a
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-06-10 17:26:31 +0800

    fix: make login page compile

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,10 +1,3 @@
-import { Button, Card, Form, Input, message } from 'antd';
-import { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useAuthStore } from '@/store/auth';
-
 export default function LoginPage(){
-  const nav = useNavigate();
-  const loc = useLocation();
-  const loginByApi = useAuthStore(s=>s.loginByApi);
-  const [loading, setLoading] = useState(false);
+  return <div>Login</div>;
+}
